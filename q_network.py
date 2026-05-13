977d70a45cd1f4519f9146136ca8967e796708e1
Weibo0123 <weibolin322@gmail.com>
2026-05-12 21:02:06 -0500

    add the class

--- q_network.py
+++ q_network.py
@@ -3,4 +3,13 @@
 import torch
 import torch.nn as nn
 
-
+class QNetwork(nn.Module):
+    def __init__(self, input_size, output_size):
+        super(QNetwork, self).__init__()
+        self.network = nn.Sequential(
+            nn.Linear(input_size, 128),
+            nn.ReLU(),
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, output_size)
+        )
