43296a9fb9e3434b512106cab90aa39d337ad4b1
Weibo0123 <weibolin322@gmail.com>
2026-05-12 22:09:28 -0500

    add the class

--- dqn_agent.py
+++ dqn_agent.py
@@ -2,4 +2,10 @@
 from q_network import QNetwork
 from experience_replay import ReplayBuffer
 
-
+class DQNAgent:
+    def __init__(self, input_size, output_size):
+        self.q_network = QNetwork(input_size, output_size)
+        self.replay_buffer = ReplayBuffer(capacity=10000)
+        self.epsilon = 1.0
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
