e626af1fdc2d64a6db44a3a7e72ce62cbd2aaec1
Weibo0123 <weibolin322@gmail.com>
2026-05-13 07:04:22 -0500

    add a function to select actions

--- dqn_agent.py
+++ dqn_agent.py
@@ -1,6 +1,8 @@
 # dqn_agent.py
 from q_network import QNetwork
 from experience_replay import ReplayBuffer
+import random
+import torch
 
 class DQNAgent:
     def __init__(self, input_size, output_size):
@@ -9,3 +11,17 @@ class DQNAgent:
         self.epsilon = 1.0
         self.epsilon_min = 0.01
         self.epsilon_decay = 0.995
+
+    def select_action(self, state, valid_actions):
+        if not valid_actions:
+            return None
+
+        if random.random() < self.epsilon:
+            return random.choice(valid_actions)
+        else:
+            state_tensor = torch.FloatTensor(state).flatten().unsqueeze(0)
+            q_values = self.q_network(state_tensor)
+            best_index = q_values.squeeze().argmax().item()
+            best_index = min(best_index, len(valid_actions) - 1)
+            return valid_actions[best_index]
+
