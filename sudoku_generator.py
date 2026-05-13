6a7c972c51456b07e5b40ec6f51cd6ba944da5fc
Weibo0123 <weibolin322@gmail.com>
2026-05-13 11:35:49 -0500

    update the sudoku_generator to support different board size

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -2,29 +2,29 @@
 import random
 from sudoku_env import SudokuEnv
 
-def generate_sudoku(board=None):
+def generate_sudoku(board=None, size=9):
     if board is None:
-        board = [[0]*9 for _ in range(9)]
+        board = [[0]*size for _ in range(size)]
 
-    empty = [(r, c) for r in range(9) for c in range(9) if board[r][c] == 0]
+    empty = [(r, c) for r in range(size) for c in range(size) if board[r][c] == 0]
     if not empty:
         return board
 
     row, col = empty[0]
-    nums = list(range(1, 10))
+    nums = list(range(1, size + 1))
     random.shuffle(nums)
 
     for num in nums:
         if SudokuEnv(board).is_valid(row, col, num):
             board[row][col] = num
-            if generate_sudoku(board):
+            if generate_sudoku(board, size):
                 return board
             board[row][col] = 0
     return None
 
-def create_sudoku(full_board, empty_cells=40):
+def create_sudoku(full_board, empty_cells=40, size=9):
     puzzle = [row[:] for row in full_board]
-    cell = [(r, c) for r in range(9) for c in range(9)]
+    cell = [(r, c) for r in range(size) for c in range(size)]
     random.shuffle(cell)
 
     for i in range(empty_cells):
--- train.py
+++ train.py
@@ -3,3 +3,5 @@ from sudoku_env import SudokuEnv
 from dqn_agent import DQNAgent
 from sudoku_generator import generate_sudoku, create_sudoku
 
+def train():
+    ...
