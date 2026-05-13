575d62e030e15f41c2144b97ed187f2e05f0ab7c
Weibo0123 <weibolin322@gmail.com>
2026-05-12 20:36:38 -0500

    add functions for pushing experience and get the sample data

--- experience_replay.py
+++ experience_replay.py
@@ -7,3 +7,9 @@ import random
 class ReplayBuffer:
     def __init__(self, capacity):
         self.buffer = deque(maxlen=capacity)
+
+    def push(self, state, action, reward, next_state, done):
+        self.buffer.append((state, action, reward, next_state, done))
+
+    def sample(self, batch_size):
+        return random.sample(self.buffer, batch_size)
