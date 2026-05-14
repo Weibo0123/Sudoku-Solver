e60f938a83c8db372b97957bf2aeb755d50c9ccf
Weibo0123 <weibolin322@gmail.com>
2026-05-14 07:20:20 -0500

    add more to the model so the model could learn more

--- imitation/model.py
+++ imitation/model.py
@@ -5,11 +5,14 @@ class ImitationModel(nn.Module):
     def __init__(self, input_size, output_size):
         super(ImitationModel, self).__init__()
         self.network = nn.Sequential(
-            nn.Linear(input_size, 128),
+            nn.Linear(input_size, 512),
             nn.ReLU(),
-            nn.Linear(128, 128),
+            nn.Linear(512, 256),
             nn.ReLU(),
-            nn.Linear(128, output_size),        )
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Linear(128, output_size)
+        )
 
     def forward(self, x):
         return self.network(x)
