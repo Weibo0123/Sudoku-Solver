fae62b4585cbadb6ba8fd839eca320aa27cfcdad
Weibo0123 <weibolin322@gmail.com>
2026-05-21 18:55:28 -0500

    add the functions to store the accuracy of each model

--- marl/agent_manager.py
+++ marl/agent_manager.py
@@ -21,3 +21,18 @@ class AgentManager:
     def box_idx(self, row: int, col: int) -> int:
         return (row // 3) * 3 + (col // 3)
 
+    def update_accuracy(self, agent_idx: int, correct: bool):
+        self.total_counts[agent_idx] += 1
+        if correct:
+            self.correct_counts[agent_idx] += 1
+
+    def get_accuracy(self, agent_idx: int) -> float:
+        if self.total_counts[agent_idx] == 0:
+            return 0.5
+        return self.correct_counts[agent_idx] / self.total_counts[agent_idx]
+
+    def reset_accuracy(self):
+        self.correct_counts = [0] * 27
+        self.total_counts   = [0] * 27
+
+
