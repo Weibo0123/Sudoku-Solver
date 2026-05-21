81f777e239b521a5f237db66cd2c737a11e703d3
Weibo0123 <weibolin322@gmail.com>
2026-05-21 18:54:56 -0500

    add the index for all the agents

--- marl/agent_manager.py
+++ marl/agent_manager.py
@@ -9,4 +9,15 @@ class AgentManager:
         self.correct_counts = [0] * 27
         self.total_counts   = [0] * 27
 
+    def row_agent_idx(self, row: int) -> int:
+        return row
+
+    def col_agent_idx(self, col: int) -> int:
+        return 9 + col
+
+    def box_agent_idx(self, box: int) -> int:
+        return 18 + box
+
+    def box_idx(self, row: int, col: int) -> int:
+        return (row // 3) * 3 + (col // 3)
 
