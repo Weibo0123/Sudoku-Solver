0e19572e1e2df79ccfb4e68946a7817ac25f820b
Weibo0123 <weibolin322@gmail.com>
2026-05-21 19:43:57 -0500

    finish the agent manager

--- marl/agent_manager.py
+++ marl/agent_manager.py
@@ -1,6 +1,8 @@
 import torch
-from marl.agent_network import AgentTransformer, AGENT_ROW, AGENT_COL, AGENT_BOX
+from torch import dtype
 
+from marl.agent_network import AgentTransformer, AGENT_ROW, AGENT_COL, AGENT_BOX
+from env.sudoku_env import SudokuEnv
 class AgentManager:
     def __init__(self, device="cpu"):
         self.device = device
@@ -57,3 +59,51 @@ class AgentManager:
                 position.append(pos)
                 pos += 1
         return cell_value, position
+
+    def _get_action_mask(self, env, row: int, col: int) -> list:
+        return [env.is_valid(row=row, col=col, value=digit) for digit in range(1, 10)]
+
+    def get_scores(self, env, board: list, target_row: int, target_col: int):
+        box = self.box_idx(target_row, target_col)
+
+        row_cells, row_pos = self._extract_row_input(board, target_row)
+        col_cells, col_pos = self._extract_col_input(board, target_col)
+        box_cells, box_pos = self._extract_box_input(board, box)
+
+        action_mask = self._get_action_mask(env, target_row, target_col)
+
+        cell_value = torch.tensor(
+            [row_cells, col_cells, box_cells], dtype=torch.long
+        ).to(self.device)
+
+        position = torch.tensor(
+            [row_pos, col_pos, box_pos], dtype=torch.long
+        ).to(self.device)
+
+        agent_types = torch.tensor(
+            [AGENT_ROW, AGENT_COL, AGENT_BOX], dtype=torch.long
+        ).to(self.device)
+
+        mask = torch.tensor(
+            [action_mask, action_mask, action_mask], dtype=torch.bool
+        ).to(self.device)
+
+        with torch.no_grad():
+            logits = self.agent_network(cell_value, position, agent_types, mask)
+
+        scores = {
+            "row": logits[0],
+            "col": logits[1],
+            "box": logits[2],
+        }
+
+        agent_indices = {
+            "row": self.row_agent_idx(target_row),
+            "col": self.col_agent_idx(target_col),
+            "box": self.box_agent_idx(box),
+        }
+
+        return scores, agent_indices, torch.tensor(action_mask, dtype=torch.bool)
+
+    def parameters(self):
+        return self.agent_network.parameters()
