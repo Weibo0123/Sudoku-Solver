7734e0d52ec40addfea9787f11e9a63dc0eb7918
Weibo0123 <weibolin322@gmail.com>
2026-06-16 20:00:35 -0500

    add more comment on dataset

--- imitation/dataset.py
+++ imitation/dataset.py
@@ -3,6 +3,28 @@ from env.sudoku_generator import generate_sudoku, create_sudoku
 from imitation.solve import solve_and_record
 
 def generate_dataset(num_puzzles=1000, size=9, empty_cells=40):
+    """
+    Generates a dataset of sudoku puzzles and their corresponding solving steps.
+
+    Parameters:
+    num_puzzles: int, optional
+        The number of sudoku puzzles to generate and record. Defaults to 1000.
+    size: int, optional
+        The dimensional size of the sudoku board (e.g., 9 for standard 9x9 sudoku).
+        Defaults to 9.
+    empty_cells: int, optional
+        The number of empty cells in each generated puzzle. Defaults to 40.
+
+    Returns:
+    tuple
+        A tuple containing three elements:
+        - X: List of float
+            A list of normalized board states, where the board cells are divided by 9.0.
+        - y_cell: List of int
+            A list of cell indices for the target cell to fill during solving.
+        - y_num: List of int
+            A list of numbers to fill corresponding to the target cell indices.
+    """
     X, y_cell, y_num = [], [], []
 
     for _ in range(num_puzzles):
